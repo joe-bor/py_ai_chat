7401ac88abca3999081f3fd0234caf177950ab87
joe-bor <kimborlongan_08@yahoo.com>
2023-12-19 15:14:19 -0800

    added imports and first api endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,53 @@
-from fastapi import FastAPI
+# uvicorn main:app
+# uvicorn main:app --reload
 
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+# Custom Function Imports
+#---
+
+
+# Initiate App
 app = FastAPI()
 
 
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000"
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+# Endpoints
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello This is the root path!"}
+
+
+@app.get("/health")
+async def check_health():
+    return {"message": "check_health() invoked"}
+
+
+# Send Audio File
+# Note: Not playing in browser when using post request
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+    print("hello")
